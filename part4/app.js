a8ccb13f5dfdfdef285f8682f7f2cf590f5e61ec
Yang Song <154292645+idgafq@users.noreply.github.com>
2024-03-02 17:08:20 +0800

    submit part 4.1-4.2

---
+++ part4/app.js
@@ -0,0 +1,26 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const { requestLogger, unknownEndpoint, errorHandler } = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+logger.info('connecting to mongodb')
+mongoose.connect(config.MONGODB_URI)
+	.then(() => logger.info('connected to mongodb'))
+	.catch((err) => logger.error('error connecting to MongoDB', err.message))
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+module.exports = app
